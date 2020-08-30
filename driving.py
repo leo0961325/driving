8d5fcb1fc8b61cdbe746e4910ffdf13c1990daf6
leo0961325 <47008876+leo0961325@users.noreply.github.com>
2020-08-30 23:18:48 +0800

    Add files via upload
    the age you can drive car

---
+++ driving.py
@@ -0,0 +1,15 @@
+country = input ("輸入你的國家 : ")
+age = int(input("輸入你的年齡 : "))
+
+if country == "台灣" :
+    if age >= 18 :
+        print("你可以考駕照")
+    else :
+        print("你還不能考駕照")
+elif country == "美國" :
+    if  age >= 16 :
+        print("你可以考駕照")
+    else : 
+        print("你不能考駕照")
+else :
+    print("Valid value")
